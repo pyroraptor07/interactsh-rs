cad9693814961ec7a19e5a9073b4fe4b948cf86f
Matt George <pyroraptor07@gmail.com>
2022-12-21 23:52:35 -0700

    fix to parselogs trait

--- src/client_next/log_parsing.rs
+++ src/client_next/log_parsing.rs
@@ -1,8 +1,12 @@
 use crate::client::http_utils::PollResponse;
 use crate::crypto::rsa::RSAPrivKey;
+use crate::interaction_log::LogEntry;
 
 pub(super) trait ParseLogs {
-    fn parse_logs(response: PollResponse, rsa_key: &RSAPrivKey) -> Result<String, snafu::Whatever> {
+    fn parse_logs(
+        response: PollResponse,
+        rsa_key: &RSAPrivKey,
+    ) -> Result<Vec<LogEntry>, snafu::Whatever> {
         todo!()
     }
 }
